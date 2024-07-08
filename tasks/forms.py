78c6792ef8bda74806600d6bdff9396a8df8f2ee
Sachi Sisodia <63452638+Siya-Sachi@users.noreply.github.com>
2024-07-08 03:27:27 -0400

    all the feautres like as per user and no need to give the user it will take the loggen in one has been added. yay

--- tasks/forms.py
+++ tasks/forms.py
@@ -4,11 +4,11 @@ from tasks.models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title','description','date','is_completed','user']
+        fields = ['title','description','date','is_completed']
         widgets={
             'title': forms.TextInput(attrs={'class':'form-control'}),
             'description': forms.Textarea(attrs={'class':'form-control'}),
             'date': forms.DateInput(attrs={'class':'form-control'}),
             'is_completed': forms.CheckboxInput(attrs={'class':'form-check'}),
-            'user': forms.Select(attrs={'class':'form-control'}),
+            # 'user': forms.Select(attrs={'class':'form-control'}),
         }
